7a9a4662309964751e0c78a1cad528c1f1aa6055
Leonardo <Leonardo@upn.pe>
2025-05-21 17:01:30 -0500

    Agregar archivos de proyecto.

---
+++ EjemploClases/Areas.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjemploClases
+{
+    public class Areas
+    {
+        //muchosmetodos- funcionalidades
+        //recomendable ponerlo publico
+        public double AreaCuadrado(double l)
+        {
+            double a;
+            a = Math.Pow(l, 2);
+            return a;
+        }
+        public double AreaTriangulo (double b, double h)
+        {
+            return (b * h) / 2;
+        }
+        static double AreaCirculo(double r)
+        {
+            //pi es una constante númerica
+            double a;
+            a = Math.PI * Math.Pow(r, 2);
+            return a;
+        }
+        static double AreaRectangulo(double b, double h)
+        {
+            double a=b*h;
+            return a;
+        }
+    }
+}
---
+++ EjemploClases/Perimetros.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjemploClases
+{
+    public class Perimetros
+    {
+        //public double PerimetroCirculo(double r)
+        //{
+        //    double p=2*Math.PI*r;
+        //    return p;
+        //}
+        //public double PerimetroRectangulo(double h, double b)
+        //{
+        //    double p = (2 * h) + (2 * b);
+        //    return p;
+        //
+        public void PerimetroCirculo(double r, ref double p)//ref= referencia, p-->espacio de memoria
+        {
+            p = 2 * Math.PI * r;//p direccion de memoria, ya no hay retorno 
+        }
+        public void PerimetroRectangulo (double b, double h, ref double p)
+        {
+            p = 2 * (b + h);
+        }
+    }
+}
