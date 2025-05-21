69b0ba2e7b2a322033745a1a61ac18902954b092
Leonardo <Leonardo@upn.pe>
2025-05-21 17:38:30 -0500

    repasar, :c

---
+++ Ejecucion/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EjemploClases;
+
+namespace Ejecucion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Perimetros p = new Perimetros();
+            double b, h,r;
+        }
+    }
+}
